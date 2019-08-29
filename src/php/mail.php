7649a491956d27fd5d7a8b7870aa68962bac0360
Tim Konieczny <timkonieczny@gmail.com>
2019-08-30 11:25:33 +1200

    Added email header injection protection

--- src/php/mail.php
+++ src/php/mail.php
@@ -3,14 +3,27 @@ include 'credentials.php';
 
 $to = $email;
 $subject = 'test';
-$message = 'test';
+$body = 'test';
 $from = 'test@timkonieczny.com';
 
-// TODO: Email header injection prevention
+// sanitize email
+$from = str_replace(array("\r", "\n"), '', $from);
+$from = filter_var($from, FILTER_VALIDATE_EMAIL);
+
+// sanitize subject
+$subject = str_ireplace(array("\r", "\n", '%0A', '%0D'), '', $subject);
+
+// sanitize body
+$body = str_replace("\n.", "\n..", $body);
+
 // TODO: Honeypot fields
 
-if (mail($to, $subject, $message, "From: foo@example.com")) {
-    echo 'Your mail has been sent successfully.';
-} else {
-    echo 'Unable to send email. Please try again.';
+if ($from) {
+    if (mail($to, $subject, $body, "From: $from")) {
+        echo 'Your mail has been sent successfully.';
+    } else {
+        echo false;
+    }
+}else{
+    echo false;
 }
