d40922de07183694a579c410b251465e0179e3d5
Tim Konieczny <timkonieczny@gmail.com>
2019-09-02 11:30:18 +1200

    Added progress calculation to grid generation

--- src/js/renderer/HexagonGrid.js
+++ src/js/renderer/HexagonGrid.js
@@ -11,6 +11,10 @@ class HexagonGrid extends Mesh {
 
         const cylinders = []
 
+        const progressTotalElements = 6 * ((rings * (rings + 1)) / 2)
+        let progressCurrentElement = 0
+        let progressLastPercentage = 0
+
         const makeRing = (level, gap, scaleY) => {
             const identity = mat4.create()
             const pi2 = Math.PI * 2
@@ -35,6 +39,11 @@ class HexagonGrid extends Mesh {
                 let interpolator = vec3.create()
                 let position = vec3.create()
                 for (let j = 0; j < level; j++) {
+                    const progressPercentage = Math.floor(++progressCurrentElement / progressTotalElements * 100)
+                    if (progressPercentage >= progressLastPercentage + 5) {
+                        progressLastPercentage = progressPercentage
+                        console.info("[grid generation] " + progressPercentage + "%")
+                    }
                     vec3.scale(interpolator, betweenVector, j / level)
                     vec3.add(position, vertex1, interpolator)
 
@@ -62,12 +71,12 @@ class HexagonGrid extends Mesh {
 
         console.info("[grid generation] creating elements")
         makeGrid(rings, gap, scaleY)
-        console.info("[grid generation] creating elements done ("+(Date.now() - time)+"ms)")
+        console.info("[grid generation] creating elements done (" + (Date.now() - time) + "ms)")
         time = Date.now()
 
         console.info("[grid generation] merging geometries")
         const hexGridGeometry = this.mergeGeometries(...cylinders)
-        console.info("[grid generation] merging geometries done ("+(Date.now() - time)+"ms)")
+        console.info("[grid generation] merging geometries done (" + (Date.now() - time) + "ms)")
 
         this.indices = hexGridGeometry.indices
         this.interleavedArray = hexGridGeometry.interleavedArray
