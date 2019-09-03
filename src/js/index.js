44a7be44f666479f428ecafe5c41abb46a496191
Tim Konieczny <timkonieczny@gmail.com>
2019-09-03 21:03:44 +1200

    Reworked UI event handling

--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,157 @@ window.addEventListener("load", async _ => {
     let distanceToLeft, width;
     const canvas = document.querySelector("#canvas")
 
-    const wrappers = {
-        headline: document.querySelector("#headline-wrapper"),
-        contact: document.querySelector("#message-wrapper"),
-        learnmore: document.querySelector("#about-wrapper"),
-        active: document.querySelector("#headline-wrapper")
+    const onButtonClick = event => {
+
+        // init
+        const wrapper = wrappers[event.currentTarget.dataset.animation]
+        const goHome = wrapper === wrappers.headline
+
+
+        // old wrapper
+
+        // remove listeners
+        if (wrappers.active) {
+            wrappers.active.buttons.forEach(button => {
+                button.listeners.forEach(listener => {
+                    button.element.removeEventListener(listener.type, listener.callback)
+                })
+            })
+        }
+
+        // move UI
+        if (goHome)
+            wrappers.active.element.style.left = (distanceToLeft + width) + "px"
+        else
+            wrappers.active.element.style.left = (distanceToLeft - width) + "px"
+        wrappers.active.element.style.opacity = 0
+        wrappers.active.element.style.visibility = "hidden"
+
+        wrappers.active.animations.forEach(animation => {    // TODO:
+            scene.endAnimation(animation.name, animation.type)
+        })
+
+        // new wrapper
+
+        // add listeners
+        wrappers.active = wrapper
+        wrappers.active.buttons.forEach(button => {
+            button.listeners.forEach(listener => {
+                button.element.addEventListener(listener.type, listener.callback)
+            })
+        })
+
+        // move UI
+        wrappers.active.element.style.left = distanceToLeft + "px"
+        wrappers.active.element.style.opacity = 1
+        wrappers.active.element.style.visibility = "visible"
+        if (goHome)
+            wrappers.learnmore.element.style.left = (distanceToLeft + width) + "px"
+
+        // 3D animations
+        if (goHome)
+            scene.endAllAnimations()
+        else {
+            wrappers.active.animations.forEach(animation => {    // TODO:
+                scene.startAnimation(animation.name, animation.type)
+            })
+        }
     }
+
+    const onButtonClickExternal = _ => {
+        console.log("go to linkedin")
+    }
+
+    const onHoverableMouseenter = event => {
+        scene.startAnimation(event.currentTarget.dataset.animation, "hover")
+    }
+
+    const onHoverableMouseexit = event => {
+        scene.endAnimation(event.currentTarget.dataset.animation, "hover")
+    }
+
+    const wrappers = {
+        headline: {
+            element: document.querySelector("#headline-wrapper"),
+            animations: [],
+            buttons: [
+                {
+                    element: document.querySelector("#headline-wrapper .contact-button"),
+                    listeners: [
+                        { type: "click", callback: onButtonClick },
+                        { type: "mouseenter", callback: onHoverableMouseenter },
+                        { type: "mouseleave", callback: onHoverableMouseexit }
+                    ]
+                },
+                {
+                    element: document.querySelector("#headline-wrapper .linkedin"),
+                    listeners: [
+                        { type: "click", callback: onButtonClickExternal },
+                        { type: "mouseenter", callback: onHoverableMouseenter },
+                        { type: "mouseleave", callback: onHoverableMouseexit }
+                    ]
+                },
+                {
+                    element: document.querySelector("#headline-wrapper #about-button"),
+                    listeners: [
+                        { type: "click", callback: onButtonClick },
+                        { type: "mouseenter", callback: onHoverableMouseenter },
+                        { type: "mouseleave", callback: onHoverableMouseexit }
+                    ]
+                }
+            ]
+        },
+        contact: {
+            element: document.querySelector("#message-wrapper"),
+            animations: [
+                { name: "contact", type: "hover" },
+                { name: "contact", type: "click" }
+            ],
+            buttons: [
+                {
+                    element: document.querySelector("#message-wrapper .back-arrow"),
+                    listeners: [
+                        { type: "click", callback: onButtonClick }
+                    ]
+                }
+            ]
+        },
+        learnmore: {
+            element: document.querySelector("#about-wrapper"),
+            animations: [
+                { name: "learnmore", type: "hover" },
+                { name: "learnmore", type: "click" }
+            ],
+            buttons: [
+                {
+                    element: document.querySelector("#about-wrapper .back-arrow"),
+                    listeners: [
+                        { type: "click", callback: onButtonClick }
+                    ]
+                },
+                {
+                    element: document.querySelector("#about-wrapper .contact-button"),
+                    listeners: [
+                        { type: "click", callback: onButtonClick }
+                    ]
+                },
+                {
+                    element: document.querySelector("#about-wrapper .linkedin"),
+                    listeners: [
+                        { type: "click", callback: onButtonClickExternal }
+                    ]
+                }
+            ]
+        },
+        active: null
+    }
+
+    wrappers.active = wrappers.headline
+    wrappers.active.buttons.forEach(button => {
+        button.listeners.forEach(listener => {
+            button.element.addEventListener(listener.type, listener.callback)
+        })
+    })
 
     const messageForm = document.querySelector("#message-wrapper form")
     const timetrapStart = Date.now()
@@ -41,56 +186,9 @@ window.addEventListener("load", async _ => {
         preloader.style.animation = "preloader 1s forwards";
     }
 
-    const moveWrappers = newActiveWrapper => {
-        const goHome = newActiveWrapper === wrappers.headline
-        if (goHome)
-            wrappers.active.style.left = (distanceToLeft + width) + "px"
-        else
-            wrappers.active.style.left = (distanceToLeft - width) + "px"
-        wrappers.active.style.opacity = 0
-        wrappers.active.style.visibility = "hidden"
-        wrappers.active = newActiveWrapper;
-        wrappers.active.style.left = distanceToLeft + "px"
-        wrappers.active.style.opacity = 1
-        wrappers.active.style.visibility = "visible"
-
-        if (goHome)
-            wrappers.learnmore.style.left = (distanceToLeft + width) + "px"
-    }
-
     const onResize = _ => {
-        distanceToLeft = wrappers.active.getBoundingClientRect().left
-        width = wrappers.active.clientWidth
-    }
-
-    const onButtonClick = event => {
-        if (event.currentTarget.dataset.animation != "linkedin")
-            moveWrappers(wrappers[event.currentTarget.dataset.animation])
-        if (event.currentTarget.dataset.animation != "learnmore") {
-            scene.endAnimation("learnmore", "click")
-            scene.endAnimation("learnmore", "hover")
-        }
-        scene.startAnimation(event.currentTarget.dataset.animation, "click")
-        scene.startAnimation(event.currentTarget.dataset.animation, "hover")
-        event.currentTarget.removeEventListener("mouseleave", onHoverableMouseexit) // TODO: disable all eventlisteners on inactive page
-        event.currentTarget.removeEventListener("mouseenter", onHoverableMouseenter)
-    }
-
-    const onBackArrowClick = _ => {
-        moveWrappers(wrappers.headline)
-        scene.endAllAnimations()
-        Array.from(document.querySelectorAll(".animated")).forEach(element => {
-            element.addEventListener("mouseenter", onHoverableMouseenter)
-            element.addEventListener("mouseleave", onHoverableMouseexit)
-        })
-    }
-
-    const onHoverableMouseenter = event => {
-        scene.startAnimation(event.currentTarget.dataset.animation, "hover")
-    }
-
-    const onHoverableMouseexit = event => {
-        scene.endAnimation(event.currentTarget.dataset.animation, "hover")
+        distanceToLeft = wrappers.active.element.getBoundingClientRect().left
+        width = wrappers.active.element.clientWidth
     }
 
     const onMessageFormSubmit = event => {
@@ -133,19 +231,6 @@ window.addEventListener("load", async _ => {
 
     window.addEventListener("resize", onResize)
     messageForm.addEventListener("submit", onMessageFormSubmit)
-
-    Array.from(document.querySelectorAll("#headline-wrapper .animated, #about-wrapper .contact-button, #about-wrapper .linkedin")).forEach(element => {
-        element.addEventListener("click", onButtonClick)
-    })
-
-    Array.from(document.querySelectorAll(".back-arrow")).forEach(element => {
-        element.addEventListener("click", onBackArrowClick)
-    })
-
-    Array.from(document.querySelectorAll(".animated")).forEach(element => {
-        element.addEventListener("mouseenter", onHoverableMouseenter)
-        element.addEventListener("mouseleave", onHoverableMouseexit)
-    })
 
     onResize()
 
