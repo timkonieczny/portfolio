fd07366a4358cfdc585e26c0fb0733af00c80173
Tim Konieczny <timkonieczny@gmail.com>
2019-09-15 16:16:30 +1200

    Added autoprefixer for CSS vendor prefixes

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -9,6 +9,7 @@ module.exports = {
                 use: [
                     "style-loader",
                     "css-loader",
+                    "postcss-loader",
                     "sass-loader",
                 ]
             },
