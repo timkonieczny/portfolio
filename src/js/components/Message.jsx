d2500132452bd384f2b4cb2827656229d68271bf
Tim Konieczny <timkonieczny@gmail.com>
2020-04-10 21:39:06 +0200

    Integrated message form via React state

--- src/js/components/Message.jsx
+++ src/js/components/Message.jsx
@@ -7,7 +7,10 @@ class Message extends Component {
         super()
         this.state = {
             errorMessage: "",
-            errorType: null
+            errorType: null,
+            status: null,
+            showForm: true,
+            showConfirmation: false
         }
         this.isFormDisabled = false
         this.isSendAnimationFinished = false
@@ -46,23 +49,6 @@ class Message extends Component {
         return true
     }
 
-    showMessageConfirmation(requestStatus) {
-        switch (requestStatus) {
-            case 200:
-                document.querySelector("#message-success").classList.add("show")
-                break
-            case 400:
-                document.querySelector("#message-client-error").classList.add("show")
-                break
-            case 500:
-                document.querySelector("#message-server-error").classList.add("show")
-                break
-            default:
-                document.querySelector("#message-unknown-error").classList.add("show")
-                break
-        }
-    }
-
     onMessageFormSubmit(event) {
         event.preventDefault()
 
@@ -81,37 +67,30 @@ class Message extends Component {
             request.addEventListener("readystatechange", event => {
                 let requestStatus
                 if (event.currentTarget.readyState == 4) {
-                    if (this.isSendAnimationFinished) {
-                        this.showMessageConfirmation(event.currentTarget.status)
-                    } else {
+                    if (this.isSendAnimationFinished)
+                        this.setState({ status: event.currentTarget.status })
+                    else {
                         requestStatus = event.currentTarget.status
                         this.hasRequestReadyState = true
                     }
                 }
                 if (this.isAnimationPending) {
                     this.isAnimationPending = false
-                    const messageForm = document.querySelector("#message-wrapper form")
-                    messageForm.classList.add("sent")
-                    const messageFormWrapper = document.querySelector("#message-form-wrapper")
-
-                    messageFormWrapper.style.overflow = "visible"
+                    this.setState({ showForm: false })
                     const onSentAnimationEnd = _ => {
-                        messageFormWrapper.style.overflow = ""
-                        document.querySelector("#message-confirmation").classList.add("show")
-                        messageForm.style.display = "none"
+                        this.setState({ showConfirmation: true })
                         if (this.hasRequestReadyState)
-                            this.showMessageConfirmation(requestStatus)
+                            this.setState({ status: event.currentTarget.status })
                         else
                             this.isSendAnimationFinished = true
-                        messageForm.removeEventListener("animationend", onSentAnimationEnd)
+                        this.messageForm.removeEventListener("animationend", onSentAnimationEnd)
                     }
-                    messageForm.addEventListener("animationend", onSentAnimationEnd)
+                    this.messageForm.addEventListener("animationend", onSentAnimationEnd)
                 }
             })
 
             request.open("POST", mailServerURL);
-
-            request.send(new FormData(document.querySelector("#message-wrapper form")));
+            request.send(new FormData(this.messageForm));
         }
     }
 
@@ -122,23 +101,34 @@ class Message extends Component {
                     <a className="back-arrow" data-animation="headline" tabIndex="1" href="#"><i
                         className="fas fa-long-arrow-alt-left"></i></a>
                 </div>
-                <div id="message-form-wrapper">
+                <div id="message-form-wrapper"
+                    style={this.state.showForm && !this.state.showConfirmation ? {} : { overflow: "visible" }}>
                     <h2>Let's have a chat.</h2>
-                    <form noValidate onSubmit={this.onMessageFormSubmit.bind(this)}>
+                    <form noValidate onSubmit={this.onMessageFormSubmit.bind(this)}
+                        className={`${this.state.showForm ? "" : "sent"}`}
+                        style={this.state.showConfirmation ? { display: "none" } : {}}
+                        ref={(element) => { this.messageForm = element }}>
                         <div className="field-wrapper">
                             <span>
-                                <input placeholder="Name" name="name" tabIndex="2" autoComplete="on" id="name-input" className={`${this.state.errorType === "name" ? "invalid" : ""}`} ref={(element) => { this.nameInput = element }} />
+                                <input placeholder="Name" name="name" tabIndex="2" autoComplete="on" id="name-input"
+                                    className={`${this.state.errorType === "name" ? "invalid" : ""}`}
+                                    ref={(element) => { this.nameInput = element }} />
                                 <span></span>
-                                <input placeholder="Email" name="emailaddress" type="email" tabIndex="3" autoComplete="on"
-                                    id="emailaddress-input" className={`${this.state.errorType === "emailaddress" ? "invalid" : ""}`} ref={(element) => { this.emailInput = element }} />
+                                <input placeholder="Email" name="emailaddress" type="email" tabIndex="3"
+                                    autoComplete="on" id="emailaddress-input"
+                                    className={`${this.state.errorType === "emailaddress" ? "invalid" : ""}`}
+                                    ref={(element) => { this.emailInput = element }} />
                             </span>
                         </div>
                         <div className="field-wrapper">
-                            <input placeholder="Subject" name="subject" autoComplete="off" tabIndex="4" id="subject-input" className={`${this.state.errorType === "subject" ? "invalid" : ""}`} ref={(element) => { this.subjectInput = element }} />
+                            <input placeholder="Subject" name="subject" autoComplete="off" tabIndex="4"
+                                id="subject-input" className={`${this.state.errorType === "subject" ? "invalid" : ""}`}
+                                ref={(element) => { this.subjectInput = element }} />
                         </div>
                         <div className="field-wrapper" id="textarea-wrapper">
                             <textarea placeholder="Message" name="message" autoComplete="off" tabIndex="5"
-                                id="message-input" className={`${this.state.errorType === "message" ? "invalid" : ""}`} ref={(element) => { this.messageInput = element }}></textarea>
+                                id="message-input" className={`${this.state.errorType === "message" ? "invalid" : ""}`}
+                                ref={(element) => { this.messageInput = element }}></textarea>
                         </div>
                         <div className={`field-wrapper ${this.state.errorType ? "hide" : ""}`} id="submit-button-wrapper">
                             <button id="send-button" type="submit" tabIndex="6">
@@ -153,13 +143,13 @@ class Message extends Component {
                         <input className="honey" placeholder="Website" name="website" tabIndex="-1" autoComplete="no" type="url" />
                         <input className="honey" placeholder="Email" name="email" tabIndex="-1" autoComplete="no" type="email" />
                     </form>
-                    <div id="message-confirmation">
-                        <div id="message-success">
+                    <div id="message-confirmation" className={`${this.state.showConfirmation ? "show" : ""}`}>
+                        <div id="message-success" className={`${this.state.status === 200 ? "show" : ""}`}>
                             <i className="fas fa-paper-plane"></i>
                             <h3>Thank you for your message.</h3>
                             <p>I'll get back to you as soon as possible!</p>
                         </div>
-                        <div id="message-client-error">
+                        <div id="message-client-error" className={`${this.state.status === 400 ? "show" : ""}`}>
                             <i className="fas fa-robot"></i>
                             <h3>400 (Bad Request)</h3>
                             <p>
@@ -167,15 +157,17 @@ class Message extends Component {
                                 later.<br />Sorry for the inconvenience.
                             </p>
                         </div>
-                        <div id="message-server-error">
+                        <div id="message-server-error" className={`${this.state.status === 500 ? "show" : ""}`}>
                             <i className="fas fa-redo-alt"></i>
-                            <h3>400 (Internal Server Error)</h3>
+                            <h3>500 (Internal Server Error)</h3>
                             <p>
                                 Uh. Oh. Something went wrong.<br />Please try again later.<br />Sorry
                                 for the inconvenience.
                             </p>
                         </div>
-                        <div id="message-unknown-error">
+                        <div id="message-unknown-error"
+                            className={`${this.state.status !== null && this.state.status !== 200
+                                && this.state.status !== 400 && this.state.status !== 500 ? "show" : ""}`}>
                             <i className="fas fa-redo-alt"></i>
                             <h3>Uh. Oh. Something went wrong.</h3>
                             <p>Please try again later.<br />Sorry for the inconvenience.</p>
