147683f3fcb8ab19974074e95b3a50938ae908c5
Tim Konieczny <timkonieczny@gmail.com>
2020-04-10 22:44:38 +0200

    Componentized message form

--- src/js/components/Message.jsx
+++ src/js/components/Message.jsx
@@ -1,97 +1,23 @@
 import React, { Component } from "react";
-import { mailServerURL } from "../URLs"
+import MessageForm from "./MessageForm";
 
 class Message extends Component {
 
     constructor() {
         super()
         this.state = {
-            errorMessage: "",
-            errorType: null,
             status: null,
-            showForm: true,
-            showConfirmation: false
+            showConfirmation: false,
+            isAnimationRunning: false
         }
-        this.isFormDisabled = false
-        this.isSendAnimationFinished = false
-        this.hasRequestReadyState = false
-        this.isAnimationPending = true
     }
 
-    componentDidMount() {
-        this.timetrapStart = Date.now()
+    onMessageSent(requestStatus) {
+        this.setState({ status: requestStatus, showConfirmation: true })
     }
 
-    onInvalidInputFocus(event) {
-        event.currentTarget.classList.remove("invalid")
-        this.setState({ errorType: null })
-        event.currentTarget.removeEventListener("focus", event.currentTarget.onInvalidInputFocus)
-    }
-
-    validateInput(input, errorMessage) {
-        if (!input.value) {
-            this.setState({ errorMessage: errorMessage, errorType: input.name })
-            input.onInvalidInputFocus = this.onInvalidInputFocus.bind(this)
-            input.addEventListener("focus", input.onInvalidInputFocus)
-            return false
-        }
-        return true
-    }
-
-    validateEmail(input, errorMessage) {
-        const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-        if (!input.value || !regex.test(input.value)) {
-            this.setState({ errorMessage: errorMessage, errorType: input.name })
-            input.onInvalidInputFocus = this.onInvalidInputFocus.bind(this)
-            input.addEventListener("focus", input.onInvalidInputFocus)
-            return false
-        }
-        return true
-    }
-
-    onMessageFormSubmit(event) {
-        event.preventDefault()
-
-        if (this.validateInput(this.nameInput, "Please enter your name.") &&
-            this.validateEmail(this.emailInput, "Please enter a valid email address.") &&
-            this.validateInput(this.subjectInput, "Please enter a subject.") &&
-            this.validateInput(this.messageInput, "Please enter a message.")) {
-
-            // anti spam timetrap
-            if (this.isFormDisabled || Date.now() - this.timetrapStart < 3000) {
-                this.isFormDisabled = true
-                return
-            }
-
-            const request = new XMLHttpRequest();
-            request.addEventListener("readystatechange", event => {
-                let requestStatus
-                if (event.currentTarget.readyState == 4) {
-                    if (this.isSendAnimationFinished)
-                        this.setState({ status: event.currentTarget.status })
-                    else {
-                        requestStatus = event.currentTarget.status
-                        this.hasRequestReadyState = true
-                    }
-                }
-                if (this.isAnimationPending) {
-                    this.isAnimationPending = false
-                    this.setState({ showForm: false })
-                    const onSentAnimationEnd = _ => {
-                        this.setState({ showConfirmation: true })
-                        if (this.hasRequestReadyState)
-                            this.setState({ status: event.currentTarget.status })
-                        else
-                            this.isSendAnimationFinished = true
-                        this.messageForm.removeEventListener("animationend", onSentAnimationEnd)
-                    }
-                    this.messageForm.addEventListener("animationend", onSentAnimationEnd)
-                }
-            })
-
-            request.open("POST", mailServerURL);
-            request.send(new FormData(this.messageForm));
-        }
+    onAnimationStarted() {
+        this.setState({ isAnimationRunning: true })
     }
 
     render() {
@@ -102,47 +28,10 @@ class Message extends Component {
                         className="fas fa-long-arrow-alt-left"></i></a>
                 </div>
                 <div id="message-form-wrapper"
-                    style={this.state.showForm && !this.state.showConfirmation ? {} : { overflow: "visible" }}>
+                    style={!this.state.showConfirmation && this.state.isAnimationRunning ? { overflow: "visible" } : {}}>
                     <h2>Let's have a chat.</h2>
-                    <form noValidate onSubmit={this.onMessageFormSubmit.bind(this)}
-                        className={`${this.state.showForm ? "" : "sent"}`}
-                        style={this.state.showConfirmation ? { display: "none" } : {}}
-                        ref={(element) => { this.messageForm = element }}>
-                        <div className="field-wrapper">
-                            <span>
-                                <input placeholder="Name" name="name" tabIndex="2" autoComplete="on" id="name-input"
-                                    className={`${this.state.errorType === "name" ? "invalid" : ""}`}
-                                    ref={(element) => { this.nameInput = element }} />
-                                <span></span>
-                                <input placeholder="Email" name="emailaddress" type="email" tabIndex="3"
-                                    autoComplete="on" id="emailaddress-input"
-                                    className={`${this.state.errorType === "emailaddress" ? "invalid" : ""}`}
-                                    ref={(element) => { this.emailInput = element }} />
-                            </span>
-                        </div>
-                        <div className="field-wrapper">
-                            <input placeholder="Subject" name="subject" autoComplete="off" tabIndex="4"
-                                id="subject-input" className={`${this.state.errorType === "subject" ? "invalid" : ""}`}
-                                ref={(element) => { this.subjectInput = element }} />
-                        </div>
-                        <div className="field-wrapper" id="textarea-wrapper">
-                            <textarea placeholder="Message" name="message" autoComplete="off" tabIndex="5"
-                                id="message-input" className={`${this.state.errorType === "message" ? "invalid" : ""}`}
-                                ref={(element) => { this.messageInput = element }}></textarea>
-                        </div>
-                        <div className={`field-wrapper ${this.state.errorType ? "hide" : ""}`} id="submit-button-wrapper">
-                            <button id="send-button" type="submit" tabIndex="6">
-                                <i className="fas fa-paper-plane"></i>
-                            </button>
-                        </div>
-                        <div className={`field-wrapper ${this.state.errorType ? "" : "hide"}`} id="invalid-input-wrapper">
-                            <i className="fas fa-exclamation-triangle"></i>
-                            <p>{this.state.errorMessage}</p>
-                        </div>
-                        <input className="honey" placeholder="Phone" name="phone" tabIndex="-1" autoComplete="no" type="tel" />
-                        <input className="honey" placeholder="Website" name="website" tabIndex="-1" autoComplete="no" type="url" />
-                        <input className="honey" placeholder="Email" name="email" tabIndex="-1" autoComplete="no" type="email" />
-                    </form>
+                    <MessageForm onMessageSent={this.onMessageSent.bind(this)}
+                        onAnimationStarted={this.onAnimationStarted.bind(this)} />
                     <div id="message-confirmation" className={`${this.state.showConfirmation ? "show" : ""}`}>
                         <div id="message-success" className={`${this.state.status === 200 ? "show" : ""}`}>
                             <i className="fas fa-paper-plane"></i>
---
+++ src/js/components/MessageForm.jsx
@@ -0,0 +1,139 @@
+import React, { Component } from "react";
+import { mailServerURL } from "../URLs"
+
+class MessageForm extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            errorMessage: "",
+            errorType: null,
+            showForm: true,
+            done: false
+        }
+        this.isFormDisabled = false
+        this.isSendAnimationFinished = false
+        this.isAnimationPending = true
+    }
+
+    componentDidMount() {
+        this.timetrapStart = Date.now()
+    }
+
+    onInvalidInputFocus(event) {
+        event.currentTarget.classList.remove("invalid")
+        this.setState({ errorType: null })
+        event.currentTarget.removeEventListener("focus", event.currentTarget.onInvalidInputFocus)
+    }
+
+    validateInput(input, errorMessage) {
+        if (!input.value) {
+            this.setState({ errorMessage: errorMessage, errorType: input.name })
+            input.onInvalidInputFocus = this.onInvalidInputFocus.bind(this)
+            input.addEventListener("focus", input.onInvalidInputFocus)
+            return false
+        }
+        return true
+    }
+
+    validateEmail(input, errorMessage) {
+        const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        if (!input.value || !regex.test(input.value)) {
+            this.setState({ errorMessage: errorMessage, errorType: input.name })
+            input.onInvalidInputFocus = this.onInvalidInputFocus.bind(this)
+            input.addEventListener("focus", input.onInvalidInputFocus)
+            return false
+        }
+        return true
+    }
+
+    onMessageFormSubmit(event) {
+        event.preventDefault()
+
+        if (this.validateInput(this.nameInput, "Please enter your name.") &&
+            this.validateEmail(this.emailInput, "Please enter a valid email address.") &&
+            this.validateInput(this.subjectInput, "Please enter a subject.") &&
+            this.validateInput(this.messageInput, "Please enter a message.")) {
+
+            // anti spam timetrap
+            if (this.isFormDisabled || Date.now() - this.timetrapStart < 3000) {
+                this.isFormDisabled = true
+                return
+            }
+
+            const request = new XMLHttpRequest();
+            let requestStatus = null
+            request.addEventListener("readystatechange", event => {
+                const done = _ => {
+                    if (requestStatus && this.isSendAnimationFinished)
+                        this.props.onMessageSent(requestStatus)
+                }
+                if (event.currentTarget.readyState == 4) {
+                    requestStatus = event.currentTarget.status
+                    done()
+                }
+                if (this.isAnimationPending) {
+                    this.isAnimationPending = false
+                    this.setState({ showForm: false })
+                    this.props.onAnimationStarted()
+                    const onSentAnimationEnd = _ => {
+                        this.isSendAnimationFinished = true
+                        this.setState({ done: true })
+                        done()
+                        this.messageForm.removeEventListener("animationend", onSentAnimationEnd)
+                    }
+                    this.messageForm.addEventListener("animationend", onSentAnimationEnd)
+                }
+            })
+
+            request.open("POST", mailServerURL);
+            request.send(new FormData(this.messageForm));
+        }
+    }
+
+    render() {
+        return (
+            <form noValidate onSubmit={this.onMessageFormSubmit.bind(this)}
+                className={`${this.state.showForm ? "" : "sent"}`}
+                style={this.state.done ? { display: "none" } : {}}
+                ref={(element) => { this.messageForm = element }}>
+                <div className="field-wrapper">
+                    <span>
+                        <input placeholder="Name" name="name" tabIndex="2" autoComplete="on" id="name-input"
+                            className={`${this.state.errorType === "name" ? "invalid" : ""}`}
+                            ref={(element) => { this.nameInput = element }} />
+                        <span></span>
+                        <input placeholder="Email" name="emailaddress" type="email" tabIndex="3"
+                            autoComplete="on" id="emailaddress-input"
+                            className={`${this.state.errorType === "emailaddress" ? "invalid" : ""}`}
+                            ref={(element) => { this.emailInput = element }} />
+                    </span>
+                </div>
+                <div className="field-wrapper">
+                    <input placeholder="Subject" name="subject" autoComplete="off" tabIndex="4"
+                        id="subject-input" className={`${this.state.errorType === "subject" ? "invalid" : ""}`}
+                        ref={(element) => { this.subjectInput = element }} />
+                </div>
+                <div className="field-wrapper" id="textarea-wrapper">
+                    <textarea placeholder="Message" name="message" autoComplete="off" tabIndex="5"
+                        id="message-input" className={`${this.state.errorType === "message" ? "invalid" : ""}`}
+                        ref={(element) => { this.messageInput = element }}></textarea>
+                </div>
+                <div className={`field-wrapper ${this.state.errorType ? "hide" : ""}`} id="submit-button-wrapper">
+                    <button id="send-button" type="submit" tabIndex="6">
+                        <i className="fas fa-paper-plane"></i>
+                    </button>
+                </div>
+                <div className={`field-wrapper ${this.state.errorType ? "" : "hide"}`} id="invalid-input-wrapper">
+                    <i className="fas fa-exclamation-triangle"></i>
+                    <p>{this.state.errorMessage}</p>
+                </div>
+                <input className="honey" placeholder="Phone" name="phone" tabIndex="-1" autoComplete="no" type="tel" />
+                <input className="honey" placeholder="Website" name="website" tabIndex="-1" autoComplete="no" type="url" />
+                <input className="honey" placeholder="Email" name="email" tabIndex="-1" autoComplete="no" type="email" />
+            </form>
+        );
+    }
+}
+
+export default MessageForm;
