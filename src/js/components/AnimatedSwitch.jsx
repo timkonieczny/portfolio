5640db70465e8614e5c70ea547e15e91e5f18ad5
Tim Konieczny <timkonieczny@gmail.com>
2020-05-01 16:29:21 +0200

    Disabled hover animation while other animation is running

--- src/js/components/AnimatedSwitch.jsx
+++ src/js/components/AnimatedSwitch.jsx
@@ -13,54 +13,65 @@ class AnimatedSwitch extends Component {
         super()
 
         this.state = {
-            isAppearing: true
+            isAppearing: true,
+            enableWebGLAnimations: false
         }
     }
 
     // FIXME: during history forward history.action === "POP" -> wrong animation
     // FIXME: back button click when there is no history triggers wrong animation
+    // FIXME: WebGL animation doesn't consider initial route
     render() {
+        const onMouseEnter = event => { if (this.state.enableWebGLAnimations) this.props.onHoverableMouseEnter(event) }
+        const onMouseLeave = event => { if (this.state.enableWebGLAnimations) this.props.onHoverableMouseLeave(event) }
         return (
             <TransitionGroup id="wrapper" ref={element => { this.wrapper = element }}
                 style={this.props.show ? { opacity: 1 } : {}}
                 childFactory={child => React.cloneElement(child,
-                    { classNames: `${this.props.history.action === "PUSH" ? "push" : "pop"}` })}>
+                    { classNames: `${this.props.history.action === "POP" ? "pop" : "push"}` })}>
                 <CSSTransition
                     key={this.props.location.key}
                     timeout={500}
-                    classNames={`${this.props.immutableLocation.action === "PUSH" ? "push" : "pop"}`}
+                    classNames={`${this.props.immutableLocation.action === "POP" ? "pop" : "push"}`}
                     appear={true}
                     onEnter={(_, isAppearing) => {
+                        console.log(isAppearing)
                         this.setState({ isAppearing: isAppearing })
+                    }}
+                    onEntered={(_, isAppearing) => {
+                        this.setState({ enableWebGLAnimations: true })
+                    }}
+                    onExit={_ => {
+                        this.setState({ enableWebGLAnimations: false })
                     }}>
                     <Switch location={this.props.location}>
                         <Route path="/about">
-                            <About mouseEnterListener={this.props.onHoverableMouseEnter}
-                                mouseLeaveListener={this.props.onHoverableMouseLeave}
+                            <About mouseEnterListener={onMouseEnter}
+                                mouseLeaveListener={onMouseLeave}
                                 clickListener={this.props.onButtonClick}
                                 isAppearing={this.state.isAppearing} />
                         </Route>
                         <Route path="/message">
-                            <Message mouseEnterListener={this.props.onHoverableMouseEnter}
-                                mouseLeaveListener={this.props.onHoverableMouseLeave}
+                            <Message mouseEnterListener={onMouseEnter}
+                                mouseLeaveListener={onMouseLeave}
                                 clickListener={this.props.onButtonClick}
                                 isAppearing={this.state.isAppearing} />
                         </Route>
                         <Route path="/privacypolicy">
-                            <PrivacyPolicy mouseEnterListener={this.props.onHoverableMouseEnter}
-                                mouseLeaveListener={this.props.onHoverableMouseLeave}
+                            <PrivacyPolicy mouseEnterListener={onMouseEnter}
+                                mouseLeaveListener={onMouseLeave}
                                 clickListener={this.props.onButtonClick}
                                 isAppearing={this.state.isAppearing} />
                         </Route>
                         <Route path="/work">
-                            <Work mouseEnterListener={this.props.onHoverableMouseEnter}
-                                mouseLeaveListener={this.props.onHoverableMouseLeave}
+                            <Work mouseEnterListener={onMouseEnter}
+                                mouseLeaveListener={onMouseLeave}
                                 clickListener={this.props.onButtonClick}
                                 isAppearing={this.state.isAppearing} />
                         </Route>
                         <Route path="/">
-                            <Home mouseEnterListener={this.props.onHoverableMouseEnter}
-                                mouseLeaveListener={this.props.onHoverableMouseLeave}
+                            <Home mouseEnterListener={onMouseEnter}
+                                mouseLeaveListener={onMouseLeave}
                                 clickListener={this.props.onButtonClick} />
                         </Route>
                     </Switch>
