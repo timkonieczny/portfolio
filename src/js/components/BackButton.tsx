e758b7202fb4cf9d6aeaef29f82348a06176bf8b
Tim Konieczny <timkonieczny@gmail.com>
2020-05-18 11:55:32 +0200

    Converted BackButton to tsx

--- src/js/components/BackButton.tsx
+++ src/js/components/BackButton.tsx
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-import { withRouter, Link } from "react-router-dom";
+import { withRouter, Link, RouteComponentProps } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class BackButton extends Component {
+type Props = RouteComponentProps & {
+    hasHistory: boolean,
+    clickListener: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void,
+    history: History,
+}
+
+class BackButton extends Component<Props> {
 
     render() {
         if (this.props.hasHistory)
             return (
-                <a className="back-arrow" data-animation="back" tabIndex="1"
+                <a className="back-arrow" data-animation="back" tabIndex={1}
                     onClick={event => { this.props.clickListener(event); this.props.history.goBack() }}>
                     <FontAwesomeIcon icon="long-arrow-alt-left" />
                 </a>)
         else return (
-            <Link to="/" className="back-arrow" data-animation="headline" tabIndex="1"
+            <Link to="/" className="back-arrow" data-animation="headline" tabIndex={1}
                 onClick={this.props.clickListener}>
                 <FontAwesomeIcon icon="long-arrow-alt-left" />
             </Link>)
