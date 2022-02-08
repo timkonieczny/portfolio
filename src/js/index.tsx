0aefa2b5c5793c43e79fe964c21e7201b2cf84eb
Tim Konieczny <hello@timkonieczny.com>
2022-02-08 13:42:29 +0000

    Upgraded dependencies

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -4,7 +4,7 @@ import React from "react"
 import "../scss/index.scss"
 import ReactGA from "react-ga"
 import * as Secret from "../../secret"
-import { library } from "@fortawesome/fontawesome-svg-core"
+import { IconDefinition, library } from "@fortawesome/fontawesome-svg-core"
 import {
     faLongArrowAltLeft, faLongArrowAltRight, faPaperPlane,
     faRobot, faRedoAlt, faExclamationTriangle, faMapMarkerAlt
@@ -16,8 +16,17 @@ import { home } from "./strings"
 
 ReactGA.initialize(Secret.analytics.google);
 
-library.add(faLongArrowAltLeft, faLongArrowAltRight, faLinkedin, faXing,
-    faPaperPlane, faExclamationTriangle, faRobot, faRedoAlt, faMapMarkerAlt)
+library.add(
+    faLongArrowAltLeft as IconDefinition,
+    faLongArrowAltRight as IconDefinition,
+    faLinkedin as IconDefinition,
+    faXing as IconDefinition,
+    faPaperPlane as IconDefinition,
+    faExclamationTriangle as IconDefinition,
+    faRobot as IconDefinition,
+    faRedoAlt as IconDefinition,
+    faMapMarkerAlt as IconDefinition
+)
 
 window.addEventListener("load", (_) => {
     const app: HTMLElement = document.querySelector("#app")
