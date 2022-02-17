d109c75c95320526e60f35c4722b33befde6d353
Tim Konieczny <hello@timkonieczny.com>
2022-02-17 17:05:25 +0000

    Transformed Wrapper to Functional Component

--- src/js/components/Wrapper.jsx
+++ src/js/components/Wrapper.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react"
+import React, { Fragment, useRef, useState } from "react"
 import Preloader from "./Preloader"
 import Canvas from "./Canvas"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
@@ -9,102 +9,94 @@ import Work from "./Work"
 import About from "./About"
 import Home from "./Home"
 
-class Wrapper extends Component {
-    constructor() {
-        super()
+const Wrapper = () => {
 
-        this.state = {
-            progress: 0,
-            animations: ["home"],
-        }
+    const [progress, setProgress] = useState(0)
+    const [animations, setAnimations] = useState(["home"])
+
+    const inputEl = useRef(null)
+
+    // const componentDidMount = () => {
+    //     window.onpopstate = () => {
+    //         const animation = this.getAnimationByLocation()
+    //         if (animations.length > 1 &&
+    //             animation === animations[animations.length - 2])
+    //             this.popAnimation()
+    //         else
+    //             this.pushAnimation(animation)
+    //     }
+    // }
+
+    // const getAnimationByLocation = () => {
+    //     switch (window.location.pathname.toLowerCase()) {
+    //         case "/":
+    //             return "home"
+    //         case "/message":
+    //             return "message"
+    //         case "/services":
+    //             return "services"
+    //         case "/privacy":
+    //             return "privacy"
+    //         case "/work":
+    //             return "work"
+    //         case "/about":
+    //             return "about"
+    //         default:
+    //             return "home"
+    //     }
+    // }
+
+    const pushAnimation = (animation) => {
+        setAnimations([...animations, animation])
+        inputEl.current.scene.startAnimation(animation)
     }
 
-    componentDidMount() {
-        window.onpopstate = () => {
-            const animation = this.getAnimationByLocation()
-            if (this.state.animations.length > 1 &&
-                animation === this.state.animations[this.state.animations.length - 2])
-                this.popAnimation()
-            else
-                this.pushAnimation(animation)
-        }
+    // const popAnimation = () => {
+    //     const updatedAnimations = [...animations]
+    //     updatedAnimations.pop()
+    //     const animation = updatedAnimations[updatedAnimations.length - 1]
+    //     setAnimations(updatedAnimations)
+    //     inputEl.scene.startAnimation(animation)
+    // }
+
+    const onButtonClick = (event) => {
+        pushAnimation(event.currentTarget.dataset.animation)
     }
 
-    getAnimationByLocation() {
-        switch (window.location.pathname.toLowerCase()) {
-            case "/":
-                return "home"
-            case "/message":
-                return "message"
-            case "/services":
-                return "services"
-            case "/privacy":
-                return "privacy"
-            case "/work":
-                return "work"
-            case "/about":
-                return "about"
-            default:
-                return "home"
-        }
-    }
-
-    pushAnimation(animation) {
-        this.setState({
-            animations: [...this.state.animations, animation]
-        })
-        this.canvas.scene.startAnimation(animation)
-    }
-
-    popAnimation() {
-        this.state.animations.pop()
-        const animation = this.state.animations[this.state.animations.length - 1]
-        this.setState({
-            animations: this.state.animations
-        })
-        this.canvas.scene.startAnimation(animation)
-    }
-
-    onButtonClick(event) {
-        this.pushAnimation(event.currentTarget.dataset.animation)
-    }
-
-    progressListener(event) {
-        let progress
+    const progressListener = (event) => {
+        let progress2
         switch (event.task) {
             case "generate":
-                progress = event.progress * .85
+                progress2 = event.progress * .85
                 break
             case "merge":
-                progress = 85 + event.progress * .15
+                progress2 = 85 + event.progress * .15
                 break
             case "complete":
-                progress = event.progress
+                progress2 = event.progress
                 break
         }
-        this.setState({ progress: progress })
+        setProgress(progress2)
     }
 
-    render() {
-        return (
-            <Fragment>
-                <Canvas onProgress={this.progressListener.bind(this)} ref={element => { this.canvas = element }} />
-                <Preloader progress={this.state.progress} />
-                <div id="wrapper" style={{ opacity: 1 }}>
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path="/services" element={<Services clickListener={this.onButtonClick.bind(this)} />} />
-                            <Route path="/message" element={<Message clickListener={this.onButtonClick.bind(this)} />} />
-                            <Route path="/privacy" element={<Privacy clickListener={this.onButtonClick.bind(this)} />} />
-                            <Route path="/work" element={<Work clickListener={this.onButtonClick.bind(this)} />} />
-                            <Route path="/about" element={<About clickListener={this.onButtonClick.bind(this)} />} />
-                            <Route path="/" element={<Home clickListener={this.onButtonClick.bind(this)} />} />
-                        </Routes>
-                    </BrowserRouter>
-                </div>
-            </Fragment >
-        )
-    }
+    return (
+        <Fragment>
+            <Canvas onProgress={progressListener} ref={inputEl} />
+            <Preloader progress={progress} />
+            <div id="wrapper" style={{ opacity: 1 }}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/services" element={<Services clickListener={onButtonClick} />} />
+                        <Route path="/message" element={<Message clickListener={onButtonClick} />} />
+                        <Route path="/privacy" element={<Privacy clickListener={onButtonClick} />} />
+                        <Route path="/work" element={<Work clickListener={onButtonClick} />} />
+                        <Route path="/about" element={<About clickListener={onButtonClick} />} />
+                        <Route path="/" element={<Home clickListener={onButtonClick} />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </Fragment >
+    )
 }
 
 export default Wrapper
